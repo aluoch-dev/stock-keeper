ea93e6d041deb227555fa89f8c361d7aef1f0d77
Aluoch Laurine <laurinealuoch7@gmail.com>
2023-11-24 22:18:26 +0300

    setup kivy widgets test

--- system/main.py
+++ system/main.py
@@ -1,12 +1,24 @@
 from kivy.app import App
-from kivy.uix.widget import Widget
+from kivy.uix.label import Label
+from kivy.uix.image import Image
 
-class PongGame(Widget):
-    pass
 
-class PongApp(App):
+class MainApp(App):
     def build(self):
-        return PongGame()
-    
+        img = Image(
+            source='/path/to/real_python.png',
+            size_hint=(1, .5),
+            pos_hint={'center_x': .5, 'center_y': .5}
+        )
+
+        label = Label(
+            text='Hello Laurie from Kivy',
+            size_hint=(.5, .5),
+            pos_hint={'center_x': .5, 'center_y': .5})
+
+        return img
+
+
 if __name__ == '__main__':
-    PongApp().run()
+    app = MainApp()
+    app.run()
